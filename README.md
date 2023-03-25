7c5f71a69a237b41cec3c1dfd8b15d4628e9e1e1
mafusuke <mafusukee@gmail.com>
2023-03-26 01:16:14 +0900

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # MarkAllAsRead
+
+## Usage
+- Long press on any of guild icons, and you will see the button to MarkAllAsRead
+
+## Install
+https://raw.githubusercontent.com/m4fn3/MarkAllAsRead/master/dist/MarkAllAsRead.js
---
+++ src/components/Settings.tsx
@@ -0,0 +1,138 @@
+import {FormRow, FormSwitch, FormSection, View, ScrollView, Image, Text} from 'enmity/components'
+import {Constants, Navigation, React, StyleSheet} from 'enmity/metro/common'
+import {Linking} from "enmity/metro/common"
+// @ts-ignore
+import {name, version} from '../../manifest.json'
+import {getIDByName} from "enmity/api/assets"
+import {getByProps} from "enmity/modules"
+
+const DMIcon = getIDByName('ic_dm')
+const ServerIcon = getIDByName('ic_forum_channel')
+const GitHubIcon = getIDByName('img_account_sync_github_white')
+const DiscordIcon = getIDByName('Discord')
+const TwitterIcon = getIDByName('img_account_sync_twitter_white')
+
+const Invites = getByProps('acceptInviteAndTransitionToInviteChannel')
+
+export default ({settings}) => {
+    const styles = StyleSheet.createThemedStyleSheet({
+        container: {
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center"
+        },
+        image: {
+            width: 70,
+            height: 70,
+            marginTop: 20,
+            marginLeft: 20
+        },
+        title: {
+            flexDirection: "column",
+
+        },
+        name: {
+            fontSize: 30,
+            paddingTop: 20,
+            paddingLeft: 20,
+            paddingRight: 30,
+            color: Constants.ThemeColorMap.HEADER_PRIMARY,
+        },
+        author: {
+            fontSize: 15,
+            paddingLeft: 50,
+            color: Constants.ThemeColorMap.HEADER_SECONDARY,
+        },
+        info: {
+            height: 45,
+            paddingTop: 3,
+            paddingBottom: 3,
+            justifyContent: "center",
+            alignItems: "center"
+        },
+        footer: {
+            color: Constants.ThemeColorMap.HEADER_SECONDARY,
+            textAlign: 'center',
+            paddingTop: 10,
+            paddingBottom: 20
+        }
+    })
+    return (
+        <ScrollView>
+            <View style={styles.container}>
+                <Image
+                    source={{uri: 'https://avatars.githubusercontent.com/u/43488869'}}
+                    style={styles.image}
+                />
+                <View style={styles.title}>
+                    <Text style={styles.name}>MarkAllAsRead</Text>
+                    <Text style={styles.author}>by mafu</Text>
+                </View>
+            </View>
+            <FormSection title="ADD CONFIRMATION">
+                <FormRow
+                    label="DM"
+                    leading={<FormRow.Icon source={DMIcon}/>}
+                    trailing={
+                        <FormSwitch
+                            value={settings.getBoolean("dm", true)}
+                            onValueChange={(value) => {
+                                settings.set("dm", value)
+                            }}
+                        />
+                    }
+                />
+                <FormRow
+                    label="Server"
+                    leading={<FormRow.Icon source={DMIcon}/>}
+                    trailing={
+                        <FormSwitch
+                            value={settings.getBoolean("server", true)}
+                            onValueChange={(value) => {
+                                settings.set("server", value)
+                            }}
+                        />
+                    }
+                />
+            </FormSection>
+            <FormSection title="INFORMATION">
+                <FormRow
+                    label="Follow me on Twitter"
+                    style={styles.info}
+                    trailing={FormRow.Arrow}
+                    leading={<FormRow.Icon source={TwitterIcon}/>}
+                    onPress={() => {
+                        Linking.openURL("https://twitter.com/m4fn3")
+                    }}
+                />
+                <FormRow
+                    label="Visit my server for help"
+                    style={styles.info}
+                    trailing={FormRow.Arrow}
+                    leading={<FormRow.Icon source={DiscordIcon}/>}
+                    onPress={() => {
+                        Invites.acceptInviteAndTransitionToInviteChannel({
+                            inviteKey: 'TrCqPTCrdq',
+                            context: {location: 'Invite Button Embed'},
+                            callback: () => {
+                                Navigation.pop()
+                            }
+                        })
+                    }}
+                />
+                <FormRow
+                    label="Check Source on GitHub"
+                    style={styles.info}
+                    trailing={FormRow.Arrow}
+                    leading={<FormRow.Icon source={GitHubIcon}/>}
+                    onPress={() => {
+                        Linking.openURL("https://github.com/m4fn3/MarkAllAsRead")
+                    }}
+                />
+            </FormSection>
+            <Text style={styles.footer}>
+                {`v${version}`}
+            </Text>
+        </ScrollView>
+    )
+}
